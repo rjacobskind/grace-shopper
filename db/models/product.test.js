a2a7cdf82c8a9ef2c7ce7fb43ea9855a9f3154cd
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-02-28 15:16:54 -0500

    product model and test complete

---
+++ db/models/product.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const db = require('APP/db')
+const Product = db.model('product')
+const {expect} = require('chai')
+
+describe('Product Model', () => {
+	before('wait for the db', () => db.didSync)
+
+    describe('schema', () => {
+
+	    // This assertion expects that the Product model will have
+	    // a 'name' column. 
+	    it('has the a name property that is a string', () => {
+	        expect(Product.attributes.name).to.be.an('object');
+	    });
+
+	 }) 
+
+    describe('validations', () => {
+
+	    it('defaults the image to be our default image', () =>{
+	    	const product = Product.build()
+	    	expect(product.image).to.be.equal('APP/public/default_potion.png')
+
+	    })
+
+	})
+
+})
