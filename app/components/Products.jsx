055284d3d209fc6bf17de16003547e7636e7d87f
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-01 14:35:25 -0500

    added product component & container

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default function (products) {
   console.log('props', products)
@@ -6,3 +7,31 @@ export default function (products) {
     <p>Hello world</p>
   )
 }
+
+
+/* 
+
+export default function (products) {
+  console.log('props', products)
+  return (
+	<div>
+		<h1>Our Poppin' Potions</h1> 
+		<ul>
+			{this.props.products ? this.props.products.map(function(product){
+				return (
+					<ul key={product.id}> 
+						<Link to={`products/${product.id}`}>
+						<img src={product.url} height="250"/>
+							<li>{ product.name }</li>
+							<li>{ product.description }</li>
+							<li>${product.price}</li> 
+						</Link>
+					</ul> 
+				)
+			}): null }
+		</ul> 
+	</div> 
+  )
+}
+
+*/ 
--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -1,24 +1,54 @@
 import axios from 'axios'
 
+export const initialState = [
+		{
+			name: 'Love Potion',
+			description: 'For falling in love.',
+			url: 'http://vignette3.wikia.nocookie.net/dungeonboss/images/3/3c/Heal_Potion_Icon.png/revision/latest?cb=20160510154959',
+			price: 500,
+			category: ['potion', 'love'],
+			id: 1, 
+		},
+		{
+			name: 'Hate Potion',
+			description: 'For falling in hate.',
+			url: 'http://cdn.edgebee.com/static/shopr2/items/UP_mana_potion.png',
+			price: 1000,
+			category: ['potion', 'hate'],
+			id: 2, 
+		},
+	]
+
+// I've renamed this action creator below, besides that it is identical: 
+
 export const productsActionCreator = products => ({
 	type: 'GET PRODUCTS',
 	allProducts: products,
 })
 
-export const initialState = [
-		{
-			name: 'Love Potion',
-			description: 'For falling in love.',
-			price: 500,
-			category: ['potion', 'love'],
-		},
-		{
-			name: 'Hate Potion',
-			description: 'For falling in hate.',
-			price: 1000,
-			category: ['potion', 'hate'],
-		},
-	]
+/*
+// ~~~~~~~~ same as above action creator, just renamed ~~~~~~~~~~~~~~~~~~
+export const getProducts = products => ({
+	type: 'GET PRODUCTS',
+	allProducts: products,
+})
+
+export const loadProducts = function () {
+  return function (dispatch) {
+    fetch('/api/products')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (products) {
+      const action = getProducts(products); // this should dispatch to our store as set products to be all the products fetched via this api request
+      dispatch(action);
+    })
+    .catch(function (err) {
+      console.error(err)
+    });
+  };
+};
+*/
 
 export default function (state = initialState, action) {
 	let newState = Object.assign({}, state)
