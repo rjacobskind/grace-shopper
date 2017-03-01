92a3e79ca8bbdf722b19a819792e647925375f12
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-01 16:29:22 -0500

    fixed products GET route so that it only sends back products that have quanity greater than zero

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,37 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-export default function (products) {
-  console.log('props', products)
-  return (
-    <p>Hello world</p>
-  )
-}
-
-
-/* 
-
-export default function (products) {
-  console.log('props', products)
+export default function (props) {
+  console.log('props', props)
   return (
 	<div>
-		<h1>Our Poppin' Potions</h1> 
+		<h1>Our Poppin' Potions</h1>
 		<ul>
-			{this.props.products ? this.props.products.map(function(product){
+			{props.products ? props.products.map(function(product){
 				return (
-					<ul key={product.id}> 
+					<ul key={product.id}>
 						<Link to={`products/${product.id}`}>
-						<img src={product.url} height="250"/>
+						<img src={product.image} height="250"/>
 							<li>{ product.name }</li>
 							<li>{ product.description }</li>
-							<li>${product.price}</li> 
+							<li>${Math.round(product.price / 100).toFixed(2)}</li>
 						</Link>
-					</ul> 
+					</ul>
 				)
 			}): null }
-		</ul> 
-	</div> 
+		</ul>
+	</div>
   )
 }
 
-*/ 
--- db/models/product.js
+++ db/models/product.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Sequelize = require('Sequelize') 
+const Sequelize = require('Sequelize')
 const db = require('APP/db')
 
 const Product = db.define('product', {
@@ -14,7 +14,7 @@ const Product = db.define('product', {
 	},
 	image: {
 		type: Sequelize.STRING,
-		defaultValue: 'APP/public/default_potion.png',
+		defaultValue: '/default_potion.png',
 	},
 	price: {
 		type: Sequelize.INTEGER,
