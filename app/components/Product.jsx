746d47916df6a9350e8e0209508335ba899f9f1d
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-02 18:16:13 -0500

    added a quantity dropdown to product page

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -10,6 +10,14 @@ export default function (props) {
   			<li>${Math.round(props.product.price / 100).toFixed(2)}</li>
   			<li>{ props.product.description }</li>
   		</ul>
+      <h4>Quantity: </h4> 
+      <select> 
+        <option>1</option> 
+        <option>2</option> 
+        <option>3</option> 
+        <option>4</option> 
+        <option>5</option> 
+      </select> 
   		<button type="button">Add to Cart</button>
   	</div> 
   )
