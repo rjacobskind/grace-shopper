5e74d292442bc04f0004be4c0655569e8947dcf8
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-02 18:11:23 -0500

    created shopping cart reducer

--- app/containers/ProductsContainer.jsx
+++ app/containers/ProductsContainer.jsx
@@ -1,7 +1,5 @@
-import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Products from '../components/Products';
-// import {productsActionCreator} from '../reducers/products'
 
 const mapStateToProps = (state, ownProps) => {
   return {
---
+++ app/reducers/shoppingCart.jsx
@@ -0,0 +1,26 @@
+
+export const addCartProduct = product => ({
+	type: 'ADD CART PRODUCT',
+	cartProduct: product,
+})
+
+
+/* 
+This is the reducer for the shopping cart component of state. 
+Its state is an array, and holds each product of a user's cart. 
+The action 'ADD CART PRODUCT' adds a product to this state array.
+*/
+export default function (state = [], action) {
+	let newState = Object.assign({}, state)
+
+	switch (action.type) {
+		case 'ADD CART PRODUCT':
+			newState = [ ...newState, action.cartProduct ]
+			break
+
+		default:
+			return state
+	}
+
+	return newState
+}
