949bb42097ff6d67e6c485bc0df387ad2d24766f
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-02 17:46:07 -0500

    product page renders on click

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
-export default function (singleProduct) {
-  console.log('props', singleProduct)
+export default function (props) {
+  console.log('*********PROPS***********', props)
   return (
-    <p>Hello world</p>
+  	<div>
+  		<h1>{ props.product.name}</h1>
+  		<img src={props.product.image} height="400"/>
+  		<ul>
+  			<li>${Math.round(props.product.price / 100).toFixed(2)}</li>
+  			<li>{ props.product.description }</li>
+  		</ul>
+  		<button type="button">Add to Cart</button>
+  	</div> 
   )
 }
--- app/containers/ProductContainer.jsx
+++ app/containers/ProductContainer.jsx
@@ -2,9 +2,8 @@ import {connect} from 'react-redux';
 import Product from '../components/Product';
 
 const mapStateToProps = (state, ownProps) => {
- console.log("STATE", state); 
   return {
-    product: state  
+    product: state.product 
 	}
 }
 
