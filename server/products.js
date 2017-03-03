6e60a76a9fb7a89c8a8c1a7c6f973281dfd72931
Raina Jacobskind <Raina.Jacobskind@gmail.com>
2017-03-03 11:27:57 -0500

    small changes to cart route

--- server/products.js
+++ server/products.js
@@ -2,14 +2,15 @@
 
 const db = require('APP/db')
 const Product = db.model('product')
+const CartProduct = db.model('cartProduct')
 
 module.exports = require('express').Router()
-	.get('/', (req, res, next) => 
+	.get('/', (req, res, next) =>
 		Product.findAll({
-			// This only fetches products that have an inventory amount that is not equal to 0 
+			// This only fetches products that have an inventory amount that is not equal to 0
 			where: {
 				inventoryAmount: {
-					$ne: 0, 
+					$ne: 0,
 				}
 			}
 
@@ -18,16 +19,17 @@ module.exports = require('express').Router()
 		.catch(next)
 	)
 	.get('/:id', (req, res, next) => {
-		var id = req.params.id; 
-		Product.findById(parseInt(id)) 
+		var id = req.params.id;
+		Product.findById(parseInt(id))
 		.then(singleProduct => {
 			res.json(singleProduct)
 		})
 		.catch(next)
-	}) 
-	.post('/:id', (req, res, next) => {
-		CartProduct.create(req.body)
-
-
 	})
-	
+	.post('/:id', (req, res, next) =>
+		CartProduct.create(req.body)
+		.then(cartItem => {
+			//cartItem.setUser() //possibly need to create session stuff to get this info
+			cartItem.setProduct(req.params.id)
+		})
+	)
