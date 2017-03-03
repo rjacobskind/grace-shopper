ee65ae8ae1a9661ed4f362abe1a469f520e88342
Raina Jacobskind <Raina.Jacobskind@gmail.com>
2017-03-03 12:02:13 -0500

    Working on some session stuff

--- server/products.js
+++ server/products.js
@@ -3,9 +3,15 @@
 const db = require('APP/db')
 const Product = db.model('product')
 const CartProduct = db.model('cartProduct')
+var seshId = 1;
 
 module.exports = require('express').Router()
-	.get('/', (req, res, next) =>
+	.get('/', (req, res, next) => {
+		if (!req.session.sessionId){
+			req.session.sessionId = seshId
+			seshId++
+		}
+		console.log(req.session, "******");
 		Product.findAll({
 			// This only fetches products that have an inventory amount that is not equal to 0
 			where: {
@@ -17,7 +23,8 @@ module.exports = require('express').Router()
 		})
 		.then(arrayOfProducts => res.json(arrayOfProducts))
 		.catch(next)
-	)
+	})
+
 	.get('/:id', (req, res, next) => {
 		var id = req.params.id;
 		Product.findById(parseInt(id))
