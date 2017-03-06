3ce9fab49f660ee5fed744e3fa414035088d6630
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-06 12:51:42 -0500

    now users can also add cart products

--- server/cart.js
+++ server/cart.js
@@ -5,17 +5,13 @@ const Product = db.model('product')
 const CartProduct = db.model('cartProduct')
 
 module.exports = require('express').Router()
-	.get('/', function(req, res, next){
-		console.log("INSIDE CART"); 
-	})
 	.post('/:productId', function(req, res, next){
-		console.log("in post!")
 		CartProduct.findOne({
 			where: {
 				product_id: req.params.productId, 
 				$or: [
 					{sessionId: req.session.sessionId}, 
-					{user_id: req.user}
+					{user_id: req.user.id}
 					]
 			}
 		})
@@ -31,19 +27,14 @@ module.exports = require('express').Router()
 					}
 				})
 				.then(updatedCartItem => {
-					console.log("updated cart item", updatedCartItem); 
 					res.send(updatedCartItem)
 				})
 				.catch(next)
 			} else {
-				console.log("in here!")
-				console.log("Cart item is", cartItem); 
-				console.log("req.body", req.body)
 				return cartItem.update({
 					quantity: req.body.quantity
 				})
 				.then(updatedCartItem => {
-					console.log("updated cart item", updatedCartItem); 
 					res.send(updatedCartItem)
 				})
 				.catch(next)
@@ -54,24 +45,3 @@ module.exports = require('express').Router()
 
 	}) 
 
-
-
-
-
-
-
-  // .post('/:productId', function(req, res, next){
-  // 		console.log("========HITTING THIS ROUTE!!!!!======")
-		// CartProduct.create(req.body)
-		// .then(function(cartItem){
-		// 	cartItem.setProduct(req.params.productId);
-		//     if (req.user){
-		//       cartItem.setUser(req.user)
-		//     } else if (req.session.sessionId){
-		// 		cartItem.update({sessionId: req.session.sessionId})
-		// 	}
-  //     		//console.log("now the cart item is ", cartItem); 
-		// })
-  //   	.then(updatedCartItem => res.send(updatedCartItem))
-  //   	.catch(next)
-  //  	}) 
