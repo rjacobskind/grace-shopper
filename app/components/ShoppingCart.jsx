701e89af1a2dbfb8f4647359da10945e8b3ab4b7
Claire Freehafer <claire.freehafer@gmail.com>
2017-03-08 10:34:34 -0500

    shopping cart page

--- app/components/ShoppingCart.jsx
+++ app/components/ShoppingCart.jsx
@@ -3,54 +3,38 @@ import { Link, browserHistory } from 'react-router';
 
 
 const ShoppingCart = function (props) {
-
-	var arr = [];
-	for (var i = 0; i < props.cart.length; i++) {
-		arr.push(props.cart[i].product_id)
+	const tableStyle = {
+		textAlign: 'center',
 	}
-	const cartProductInfo = props.products.filter(function(product){
-		if (arr.indexOf(product.id) !== -1){
-			var id = product.id;
-			for (var j = 0; j < props.cart.length; j++){
-				if (props.cart[j].productId === id){
-					var quant = props.cart[j].quantity;
-				}
-			}
-		}
 
-		//return arr.indexOf(product.id) !== -1
-	})
-
-	cartProductInfo.forEach(function (product) {
-		product.cartQuantity =
-	})
-
-	return(
+	return (
 		<div>
-			<div id="table">
-				<div id="header">
-					<div id="name"><h2>Product</h2></div>
-					<div id="quantity"><h2>Quantity</h2></div>
-					<div id="price"><h2>Price</h2></div>
-				</div>
-				<div id="body">
-					{props.cart ? cartProductInfo.map(function (product) {
+			<table>
+				<tbody style={tableStyle}>
+				<tr id="header">
+					<td id="name"><h2>Product</h2></td>
+					<td id="quantity"><h2>Quantity</h2></td>
+					<td id="price"><h2>Price</h2></td>
+				</tr>
+					{props.cartProducts ? props.cartProducts.map(function (product, index) {
 						return (
-							<div className="row" key={product.id}>
-								<div className="product-info">
-									<h2>{product.name}</h2>
-								</div>
-								<div className="product-quantity">
-									<p>{product.quantity}</p>
-								</div>
-								<div className="product-price">
-									<p>{Math.round(product.price / 100).toFixed(2)}</p>
-								</div>
-							</div>
+							<tr className="row" key={product.id}>
+								<td className="product-info">
+									<img src={product.image} width="100" style={{float: 'left'}} />
+									<h2><Link to={`/products/${product.id}`}></Link>{product.name}</h2>
+								</td>
+								<td className="product-quantity">
+									<p>{props.cartProductQuantity[index]}</p>
+								</td>
+								<td className="product-price">
+									<p>{(Math.round((product.price * props.cartProductQuantity[index]) / 100).toFixed(2))}</p>
+								</td>
+							</tr>
 						)
 					}) : null}
-				</div>
-			</div>
+				</tbody>
+
+			</table>
 		</div>
 	)
 }
--- app/containers/ShoppingCartContainer.jsx
+++ app/containers/ShoppingCartContainer.jsx
@@ -2,8 +2,25 @@ import { connect } from 'react-redux'
 import ShoppingCart from '../components/ShoppingCart'
 
 const mapStateToProps = (state, ownProps) => {
-	console.log('state', state)
-	return state
+
+	const cartProductIds = []
+	for (let i = 0; i < state.cart.length; i++) {
+		cartProductIds.push(state.cart[i].product_id)
+	}
+
+	const cartProducts = state.products.filter(function (product) {
+		return cartProductIds.indexOf(product.id) !== -1
+	})
+
+	const cartProductQuantities = state.cart.map(function (product) {
+		return product.quantity
+	})
+
+
+	return {
+		cartProducts: cartProducts,
+		cartProductQuantity: cartProductQuantities
+	}
 }
 
 export default connect(mapStateToProps)(ShoppingCart)
